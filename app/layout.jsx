382789ca6ef734b7bda8eafdf4598301c7b005e3
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-07 01:54:22 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,6 @@
-"use client";
-
-import { Canvas } from "@react-three/fiber";
-import SpaceEffects from "@/components/SpaceEffect";
-
 export default function RootLayout({ children }) {
   return (
-    <main className="h-screen w-full bg-black">
-      <Canvas className="h-full w-full" camera={{ position: [0, 0, 10], fov: 75 }}>
-        <color attach="background" args={["#000000"]} />
-        <SpaceEffects />
-      </Canvas>
-      <div className="relative z-10">{children}</div>
+    <main className="h-screen w-full bg-black">{children}
     </main>
   );
 }
