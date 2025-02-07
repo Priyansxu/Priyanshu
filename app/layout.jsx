b200ae07cd570efc9d8378b136bbba5862d1e716
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-07 17:02:42 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,7 +10,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="relative w-full h-full overflow-hidden">
-        <Aurora colorStops={["#00d8ff", "#7cff67", "#ff7f7f"]} speed={0.5} />
         <div className="absolute inset-0 z-10">{children}</div>
       </body>
     </html>
