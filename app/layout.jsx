9843e5702e1530a84810bfdaebbe7ca3b75abd4c
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-07 13:56:01 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import Space from "@/components/SpaceEffects"
 
 export const metadata = {
   title: '',
@@ -8,7 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+     <Space>{children}</Space>
+      </body>
     </html>
   );
 }
