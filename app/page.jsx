4489657d08cbba455ff7a85928b63a8d9d8ad6a0
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-07 13:53:20 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,21 @@
 "use client"
 
 import Space from "@/components/SpaceEffects"
+import ASCIIText from '@/components/text/ASCIIText';
 
 export default function Page() {
   return (
     <main className="relative">
       <Space />
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
-        <h1 className="text-4xl font-bold mb-4">Priyanshu Gupta</h1>
-        <p className="text-xl">You may know other things</p>
-      </div>
+  <div>
+    <ASCIIText
+  text='Priyanshu Gupta'
+  enableWaves={true}
+  asciiFontSize={8}
+  className="m-6 left-6"
+     />
+   </div>
     </main>
   )
 }
+
