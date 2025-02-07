6f0946c27cfa43523f37d2b148c2c0c8fad0a2bf
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-07 14:07:24 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,16 +4,15 @@ import ASCIIText from '@/components/text/ASCIIText';
 
 export default function Page() {
   return (
-    <main className="relative">
-  <div>
-    <ASCIIText
-  text='Priyanshu Gupta'
-  enableWaves={true}
-  asciiFontSize={4}
-  className="m-6 left-6 z-20"
-     />
-   </div>
+    <main className="relative min-h-screen flex items-center justify-center">
+      <div className="w-full">
+        <ASCIIText
+          text='Priyanshu Gupta'
+          enableWaves={true}
+          asciiFontSize={4}
+          className="relative m-6"
+        />
+      </div>
     </main>
-  )
+  );
 }
-
