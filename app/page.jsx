93c5c1faa26eddd7bbeb6bdc68251bc138f32cd4
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-07 17:12:57 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Aurora from "@/components/Aurora"
 
 export default function Page() {
   return (
@@ -7,7 +6,7 @@ export default function Page() {
    <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
    </Head>
-    <Aurora colorStops={["#00d8ff", "#7cff67", "#ff7f7f"]} speed={0.5} />
+    <h1>Priyanshu Gupta</h1>
     </main>
   );
 }
