c9a2e75147abe8a0e8fe9cd4b0b1d5e3eb98d1e1
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-07 14:47:03 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,14 @@
 "use client";
 
+import Head from "next/head";
 import ASCIIText from '@/components/text/ASCIIText';
 
 export default function Page() {
   return (
     <main className="relative min-h-screen flex">
+   <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   </Head>
       <div>
         <ASCIIText text='Priyanshu' enableWaves={true} className="relative m-6" />
       </div>
