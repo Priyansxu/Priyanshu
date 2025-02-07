aaab1ee6e6181652259ac40599856ac4b1401997
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-07 15:15:50 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@
 
 import Head from "next/head";
 import ASCIIText from '@/components/text/ASCIIText';
+import TrueFocus from '@/components/text/TrueFocus';
 
 export default function Page() {
   return (
@@ -11,7 +12,16 @@ export default function Page() {
    </Head>
       <div>
         <ASCIIText text='Priyanshu' enableWaves={true} className="relative m-6" />
-      </div>
+        <TrueFocus 
+sentence="Priyanshu Gupta"
+manualMode={false}
+blurAmount={5}
+borderColor="red"
+animationDuration={2}
+pauseBetweenAnimations={1}
+className="relative m-6"
+/>
+         </div>
     </main>
   );
 }
