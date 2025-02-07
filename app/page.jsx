a843c84881918ae8cfbd3bb197832ea661dad9f3
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-07 17:00:30 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,4 @@
-"use client";
-
 import Head from "next/head";
-import ASCIIText from '@/components/text/ASCIIText';
-import TrueFocus from '@/components/text/TrueFocus';
-import BlurText from "@/components/text/BlurText";
 
 export default function Page() {
   return (
@@ -11,9 +6,6 @@ export default function Page() {
    <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
    </Head>
-      <div>
-        <BlurText text="Priyanshu Gupta" className="text-2xl m-8 text-white font-bold" />
-         </div>
     </main>
   );
 }
