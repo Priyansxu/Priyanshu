8b64daf53c74d602af60ec0f776cfc714e943c85
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-11 13:50:50 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,10 +3,13 @@ import Aurora from "@/components/Aurora"
 
 export default function Page() {
   return (
-    <main className="relative min-h-screen flex">
-    <div>
-    <h1>Priyanshu Gupta</h1>
-    </div>
+    <main className="min-h-screen">
+     <section className="h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold mb-4">Priyanshu Gupta</h1>
+        <p className="text-xl">Web Developer & Designer</p>
+      </div>
+    </section>
     </main>
   );
 }
