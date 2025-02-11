5e5dbb2fe16adce1562a3b2cae585041c8d70296
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-12 02:23:18 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,21 @@
 import Head from "next/head";
 import Aurora from "@/components/Aurora"
+import BlurText from "@/components/BlurText";
+
 
 export default function Page() {
   return (
     <main className="min-h-screen">
      <section className="h-screen flex items-center justify-center">
       <div className="text-center text-white font-ranade">
-        <h1 className="text-3xl font-bold mb-4">Priyanshu Gupta</h1>
+<BlurText
+  text="Priyanshu Gupta"
+  delay={150}
+  animateBy="words"
+  direction="top"
+  onAnimationComplete={handleAnimationComplete}
+  className="text-3xl font-bold mb-4"
+/>
         <p className="text-md">Web Developer & Designer</p>
       </div>
     </section>
