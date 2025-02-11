289edd8ee3df030b20b82a35fd8d32e882b68b7c
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-12 02:25:05 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -8,14 +8,7 @@ export default function Page() {
     <main className="min-h-screen">
      <section className="h-screen flex items-center justify-center">
       <div className="text-center text-white font-ranade">
-<BlurText
-  text="Priyanshu Gupta"
-  delay={150}
-  animateBy="words"
-  direction="top"
-  onAnimationComplete={handleAnimationComplete}
-  className="text-3xl font-bold mb-4"
-/>
+      <BlurText text="Priyanshu Gupta" className="text-3xl font-bold mb-4" />
         <p className="text-md">Web Developer & Designer</p>
       </div>
     </section>
