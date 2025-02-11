8eb6f04a5d02a388b5b35699a7e7016b2f4ffd5b
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-12 01:55:43 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <main className="min-h-screen">
      <section className="h-screen flex items-center justify-center">
-      <div className="text-center text-white">
+      <div className="text-center text-white font-ranade">
         <h1 className="text-6xl font-bold mb-4">PRIYANSHU GUPTA</h1>
         <p className="text-xl">Web Developer & Designer</p>
       </div>
