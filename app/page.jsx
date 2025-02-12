e70076599229f5b24f7cc6c7f4d7652e6e17623d
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-13 00:49:33 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import Aurora from "@/components/Aurora"
 import SplitText from "@/components/text/SplitText";
 import TextPressure from "@/components/text/TextPressure";
+import Fade from "@components/effects/Fade";
 
 
 export default function Page() {
@@ -12,7 +13,11 @@ export default function Page() {
      <section className="h-screen flex items-center justify-center">
       <div className="text-center text-white font-ranade">
       <SplitText text="PRIYANSHU G" className="text-4xl font-bold tracking-widest font-monaBold mb-4" />
-        <TextPressure text="Web Developer & Designer" minFontSize={16} textColor="#ffffff" />
+        import FadeContent from './FadeContent'
+  
+<Fade blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
+  <TextPressure text="Web Developer & Designer" minFontSize={16} textColor="#ffffff" />
+</Fade>
       </div>
     </section>
     </main>
