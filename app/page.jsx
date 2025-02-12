21fed9483dc823908f490d0f0253b815c6c1953e
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-12 09:25:56 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ export default function Page() {
     <main className="min-h-screen">
      <section className="h-screen flex items-center justify-center">
       <div className="text-center text-white font-ranade">
-      <SplitText text="Priyanshu Gupta" className="text-3xl font-bold mb-4" />
+      <SplitText text="Priyanshu Gupta" className="text-3xl font-bold font-monaBold mb-4" />
         <p className="text-md">Web Developer & Designer</p>
       </div>
     </section>
