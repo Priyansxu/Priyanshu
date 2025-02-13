f43fc48607df18bbc9410ab2654df2051504c028
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-13 11:33:00 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import Head from "next/head";
 import Aurora from "@/components/Aurora"
 
 export const metadata = {
@@ -10,6 +11,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#050505" />
+        <meta name="title" content="Vexel" />
+        <meta name="description" content="AI-powered Discord bot that generates images analyzes visuals and engages in smart conversations." />
+        <meta name="keywords" content="Discord bot, AI" />
+        <meta property="og:image" content="/logo.png" />
+       </Head>
         <div className="relative min-h-screen w-full">
           <div className="absolute inset-0 z-0">
             <Aurora 
