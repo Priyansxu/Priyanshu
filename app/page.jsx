42434422f04cbfdf1421c82ec8299fa326def8ae
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-13 15:50:43 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -23,8 +23,8 @@ export default function Page() {
     <Image
         src="/icon.png"
         alt="Icon"
-        width={300}
-        height={300}
+        width={100}
+        height={100}
         priority
       />
     <SplashCursor />
