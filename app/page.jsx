8a7dedc07bc4bd9f94d2735b4fccedf7aed82dd9
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-13 11:27:07 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -12,9 +12,9 @@ export default function Page() {
     <main className="min-h-screen">
      <section className="h-screen flex items-center justify-center">
       <div className="text-center text-white font-ranade">
-      <SplitText text="PRIYANSHU G" className="text-3xl font-bold tracking-widest font-monaBold mb-4" />
+      <SplitText text="PRIYANSHU G." className="text-3xl font-bold tracking-widest font-monaBold mb-4" />
    <Fade blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
-   <TextPressure text="Web Developer & Designer" minFontSize={12} textColor="#ffffff" />
+   <TextPressure text="Web Developer & Designer" minFontSize={14} textColor="#ffffff" />
   </Fade>
       </div>
     </section>
