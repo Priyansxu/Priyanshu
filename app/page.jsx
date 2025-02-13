aa7e89c3128bb996f0fb88e50faa9bb169b2f30b
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-13 15:52:57 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,39 +2,38 @@
 
 import Head from "next/head";
 import Image from "next/image";
-import Aurora from "@/components/Aurora"
+import Aurora from "@/components/Aurora";
 import SplitText from "@/components/text/SplitText";
 import TextPressure from "@/components/text/TextPressure";
 import Fade from "@/components/effects/Fade";
 import SplashCursor from "@/components/effects/SplashCursor";
 
-
 export default function Page() {
   return (
     <main className="min-h-screen">
-       <Head>
+      <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#050505" />
         <meta name="title" content="Priyanshu G." />
         <meta name="description" content="Somthing they don't know" />
         <meta name="keywords" content="Portfolio" />
-       </Head>
-     <section className="h-screen flex items-center justify-center">
-    <Image
-        src="/icon.png"
-        alt="Icon"
-        width={100}
-        height={100}
-        priority
-      />
-    <SplashCursor />
-      <div className="text-center text-white font-ranade">
-      <SplitText text="PRIYANSHU GUPTA" className="text-2xl font-bold font-monaBold mb-4" />
-   <Fade blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
-   <TextPressure text="Web Developer & Designer" minFontSize={20} textColor="#ffffff" />
-  </Fade>
-      </div>
-    </section>
+      </Head>
+      <section className="h-screen flex flex-col items-center justify-center">
+        <Image
+          src="/icon.png"
+          alt="Icon"
+          width={100}
+          height={100}
+          priority
+        />
+        <SplashCursor />
+        <div className="text-center text-white font-ranade">
+          <SplitText text="PRIYANSHU GUPTA" className="text-2xl font-bold font-monaBold mb-4" />
+          <Fade blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
+            <TextPressure text="Web Developer & Designer" minFontSize={20} textColor="#ffffff" />
+          </Fade>
+        </div>
+      </section>
     </main>
   );
 }
