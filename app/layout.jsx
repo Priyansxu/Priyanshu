1a6fc4611778ca0451bc25c02332f530617e2ffc
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-14 20:45:34 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import Head from "next/head";
 import Aurora from "@/components/Aurora"
+import LiquidChrome from "@/components/LiquidChrome";
 
 export const metadata = {
   title: 'Priyanshu G.',
@@ -13,10 +14,7 @@ export default function RootLayout({ children }) {
       <body>
         <div className="relative min-h-screen w-full">
           <div className="absolute inset-0 z-0">
-            <Aurora 
-              colorStops={["#00d8ff", "#7cff67", "#00d8ff"]}
-              amplitude={1.0}
-            />
+            <LiquidChrome />
           </div>
           <div className="relative z-10">
             {children}
