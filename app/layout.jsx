2033bc0df57ea7c0d56808c8883d6b6b69cc86a4
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-16 12:15:57 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import './globals.css';
 import Head from "next/head";
-import Aurora from "@/components/Aurora"
+import Cursor from "@/components/effects/Cursor";
 import LiquidChrome from "@/components/LiquidChrome";
 
 export const metadata = {
@@ -13,6 +13,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <div className="relative min-h-screen w-full bg-black">
+        <Cursor />
           <div className="absolute opacity-20 inset-0 z-0">
             <LiquidChrome />
           </div>
