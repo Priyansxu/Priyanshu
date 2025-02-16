af293bd4e7d49f3e702550987d072eee597b5659
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-16 12:10:31 +0530

    Update TextPressure.jsx

--- app/components/text/TextPressure.jsx
+++ app/components/text/TextPressure.jsx
@@ -127,7 +127,7 @@ const TextPressure = ({
           };
 
           const wdth = width ? Math.floor(getAttr(d, 5, 200)) : 100;
-          const wght = weight ? Math.floor(getAttr(d, 100, 900)) : 400;
+          const wght = weight ? Math.floor(getAttr(d, 200, 900)) : 400;
           const italVal = italic ? getAttr(d, 0, 1).toFixed(2) : 0;
           const alphaVal = alpha ? getAttr(d, 0, 1).toFixed(2) : 1;
 
