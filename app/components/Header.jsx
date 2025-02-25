b2d148f1689955abff8ae00683168b5cf3809b3c
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-25 14:44:43 +0530

    Update Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="fixed top-0 z-50 mx-4 mt-4  w-[90%] sm:w-[75%] md:w-[40%] lg:w-[45%] xl:w-[35%] max-w-3xl mx-auto inset-0">
       <nav className="backdrop-blur-md bg-black/20 rounded-3xl px-6 py-4 flex justify-between items-center">
-       <img src="/icon.png" alt="icon" className="h-8" />
+       <img src="/icon.svg" alt="icon" className="h-8" />
         <ul className="flex space-x-6">
           <li>
             <a href="#about" className="text-gray-300 hover:text-white transition-colors">
