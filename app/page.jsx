9d8223cbc88339ae02f6dd80ac7add968b9d8209
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-02-26 23:48:28 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -25,7 +25,7 @@ export default function Page() {
         </div>
       </section>
 
-      <section id="about" className="mt-64 px-6 md:px-8 lg:px-16 text-white">
+      <section id="about" className="pb-32 px-6 md:px-8 lg:px-16 text-white">
         <h2 className="text-3xl font-bold font-kubo mb-8 text-center">About</h2>
         <p className="max-w-2xl mx-auto text-md text-white/80 text-center font-ranadeMd">
           I'm a passionate web developer and designer with a keen eye for detail and a love for creating beautiful,
@@ -34,7 +34,7 @@ export default function Page() {
         </p>
       </section>
 
-      <section id="skills" className="mt-64 px-4 md:px-8 lg:px-16 text-white">
+      <section id="skills" className="py-32 px-4 md:px-8 lg:px-16 text-white">
         <h2 className="text-3xl font-bold font-kubo mb-8 text-center">Skills</h2>
         <div className="flex flex-wrap justify-center gap-4 max-w-3xl mx-auto">
           {["Python", "JavaScript", "React", "Next.js", "Tailwind CSS", "UI/UX Design"].map(
@@ -47,7 +47,7 @@ export default function Page() {
         </div>
       </section>
 
-      <section id="social" className="mt-64 px-4 md:px-8 lg:px-16 pb-32">
+      <section id="social" className="py-32 px-4 md:px-8 lg:px-16 pb-16">
         <h2 className="text-3xl text-white font-bold font-kubo mb-8 text-center">Social</h2>
         <div className="flex flex-wrap justify-center gap-4 max-w-2xl mx-auto">
           {[
