7a3f31179fc002caf5fa2c0e6195a350f546adcd
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-03-11 23:40:53 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -15,7 +15,6 @@ export default function Page() {
         <meta name="description" content="They don't know me son." />
         <meta name="keywords" content="Priyanshu Gupta, priyansxu, developer, programmer, designer, portfolio" />
         <meta name="theme-color" content="#080808" />
-        <meta name="google-site-verification" content="q2af5vwuRxGXH1zFQm0bHt5z5OcJZflkfwZuYgGU3l4" />
         <link rel="icon" href="/icon.svg" />
       </Head>
 
