d7c2ef9c533d1fe9f1626d63f03964f183660137
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2025-03-11 23:40:19 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta charSet="UTF-8" />
+        <title>{metadata.title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords.join(', ')} />
+        <meta name="theme-color" content={metadata.themeColor} />
+        <link rel="icon" href={metadata.icons.icon} type="image/svg+xml" />
+        <meta name="google-site-verification" content="q2af5vwuRxGXH1zFQm0bHt5z5OcJZflkfwZuYgGU3l4" />
+      </Head>
       <body>
         <div className="relative min-h-screen w-full bg-black">
           <div className="absolute opacity-20 inset-0 z-0">
