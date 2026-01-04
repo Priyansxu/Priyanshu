7d7e8161a34c274993cfc3243965ef2a6291817c
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2026-01-04 23:56:53 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -95,7 +95,7 @@ export default function Page() {
           <div className="group cursor-crosshair">
             <h1 className="text-xl font-medium font-mono flex items-center gap-3">
               Priyanshu
-              <span className="text-[10px] px-2 py-1 rounded-full border border-border bg-muted/50 text-muted-foreground tracking-[0.2em] font-bold">
+              <span className="text-[10px] px-4 py-0.5 rounded-full border border-border bg-muted/50 text-muted-foreground tracking-[0.2em] font-bold">
                 dev
               </span>
             </h1>
