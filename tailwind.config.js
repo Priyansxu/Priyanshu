e5065162c4935ac3ba45e05b251905bc4f0bf60f
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2026-01-09 15:08:49 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@
 export default {
   darkMode: "class",
   content: [
-    "./app/**/*.{js, jsx}",
-    "./components/**/*.{js, jsx}",
+    "./app/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
   ],
   theme: {
     extend: {
